e155ad1f460eb3d5a8a0e29707958087f4449a4f
Ryan <ryup2544@gmail.com>
2025-09-01 15:59:27 -0700

    got sliding bar to work

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "./styles.css"
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 export default function Navbar(){
     const [isOpen, setIsOpen] = React.useState(false)
@@ -10,13 +11,15 @@ export default function Navbar(){
     }
 
     return(
+        
         <div>
-            <nav className="mx-auto bg-sky-950 text-white">
+            <button className="fa-solid fa-bars scale-150 cursor-pointer pl-2 pt-2 peer"/>
+            <nav className="peer-focus:left-0 peer-focus:bg-sky-950 peer-focus:text-white -left-1/6 bg-white text-black h-screen w-1/6 bg-sky-950 pl-2 pt-2 fixed top-0 transition-all duration-1000">
                 <img src="" alt="logo" className="w-15 h-15" />
                 <ul>
                     <li>
-                        <a onClick={openResources}>Tài Liệu</a>
-                        <ul className={`${resource ? 'visible': 'hidden'}`}>
+                        <a onClick={openResources} className="underline hover:cursor-pointer">Tài Liệu</a>
+                        <ul className={`${resource ? 'block': 'hidden'}`}>
                             <li>Bài Hát</li>
                             <li>Trò Chơi</li>
                         </ul>
