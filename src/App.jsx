730143de9f716f947bc2a359ee5e152df48b36bc
Ryan <ryup2544@gmail.com>
2025-09-26 16:00:17 -0700

    started element tracker

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Hero"
 import Activity from "./Activity";
 import Footer from "./Footer";
 import Location from './Location'
+import Games from "./Games";
 import {Routes, Route} from 'react-router-dom';
 import "./styles.css"
 
@@ -11,7 +12,7 @@ const Main = () => (
   <Routes>
     <Route exact path='/' element={<><Hero/><Activity/><Footer/></>}></Route>
     <Route exact path='/songs' element={<Footer/>}></Route>
-    <Route exact path='/games' element={<Footer/>}></Route>
+    <Route exact path='/games' element={<Games />}></Route>
     <Route exact path='/location' element={<><Location/></>}></Route>
   </Routes>
 )
