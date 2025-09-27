8addae040c7ff496b6bba0f178f41426ec86b0b7
Ryan <ryup2544@gmail.com>
2025-09-26 21:21:50 -0700

    fixed navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 
 export default function Navbar(){
     const [isOpen, setIsOpen] = React.useState(false)
-    const [resource, setResource] = React.useState(false)
 
     return(
         
@@ -21,33 +20,21 @@ export default function Navbar(){
 
             {isOpen && (
                 <div
-                    className={`fixed inset-0 bg-black/30`}
+                    className={`fixed inset-0 bg-black/30 z-10`}
                     onClick={() => setIsOpen(false)}
-                />
-            )}
-
-            {resource && (
-                <div
-                    className={`fixed inset-0`}
-                    onClick={() => setResource(false)}
                 />
             )}
 
             
             <nav className={`${isOpen ? "left-0" : "-left-1/6"} 
                 h-screen fixed top-0 transition-all duration-500 
-                ${isOpen ? "w-1/6 bg-[#384264] text-white" : "w-1/6 bg-white text-black"}
-                sm:w-screen sm:h-1/12 sm:!left-0 sm:!bg-[#384264] sm:text-white sm:flex sm:items-center`}>
+                ${isOpen ? "w-1/2 bg-[#384264] text-white" : "w-1/6 bg-white text-black"}
+                sm:w-screen sm:h-1/12 sm:!left-0 sm:!bg-[#384264] sm:text-white sm:flex sm:items-center sm:z-3`}>
                 <NavLink to="/"><img src="./images/Hoa_Sen.jpg" alt="logo" className="w-15 h-15 bg-white rounded-full ml-2" /></NavLink>
                 <ul className="sm:flex sm:items-center sm:justify-end sm:w-full sm:space-x-6 sm:pr-7">
-                    <li className=" relative leading-none">
-                        <a onClick={() => setResource(!resource)} className="underline hover:cursor-pointer">Tài Liệu</a>
-                        <ul className={`${resource ? 'block': 'hidden'} absolute left-1/2 -translate-x-1/2 mt-2 w-40 rounded-md bg-white shadow-lg ring-1 ring-black/5 z-50`}>
-                            <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"><NavLink to="/songs">Bài Hát</NavLink></li>
-                            <li className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"><NavLink to="/games">Trò Chơi</NavLink></li>
-                        </ul>
-                    </li>
-                    <li className="leading-none"><NavLink to="/location" className="inline-flex items-center">Địa Điểm</NavLink></li>
+                    <li className="leading-none"><NavLink to="/songs" className="inline-flex items-center text-lg">Bài Hát</NavLink></li>
+                    <li className="leading-none"><NavLink to="/games" className="inline-flex items-center text-lg">Trò Chơi</NavLink></li>
+                    <li className="leading-none"><NavLink to="/location" className="inline-flex items-center text-lg">Địa Điểm</NavLink></li>
                 </ul>
             </nav>
 
--- src/Songs.jsx
+++ src/Songs.jsx
@@ -5,63 +5,65 @@ import "./styles.css"
 
 export default function Songs(){
     return(
-        <div className='sm:pt-[8.333vh] space-y-3 text-center'>
-            <Song 
-                title="Trầm Hương Đốt"
-                lyrics="Trầm hương đốt, xông ngát mười phương
-                Nguyện nguyện kính đức Nghiêm Từ vô lượng
-                Cầu cầu xin chứng tâm thành chúng con
-                Vần vần khói kết mây lành cúng dường
-                Ðạo nhiệm màu đã lang truyền nơi cùng nơi
-                Nhờ chân lý chúng sanh đều thoát luân hồi
-                Ðồng qúy kính qùy dưới đài sen
-                Dâng hoa thơm tinh khiết màu thắm tươi cành
-                Ðài quang minh sáng huy hoàng trang nghiêm
-                Ơn mười phương điều ngự hào quang an lành
-                Nhìn đạo uyển chuyễn soi khắp cùng quần sanh
-                Phật đạo đồng cùng nhau tu tin tấn mau viên thành
-                Nam mô Bổn Sư Thích Ca Mâu Ni Phật
-                Nam mô Bổn Sư Thích Ca Mâu Ni Phật"
-            />
-            <Song
-                title="Bốn Phương Trời"
-                lyrics="Bốn phương trời ta về đây chung vui,
-                Không phân chia giọng nói tiếng cười,
-                Cùng nắm tay ta kết tình thân ái,
-                Trao cho nhau những gì thiết tha (2 lần)"
-            />
-            <Song 
-                title="Anh Em Ta Về"
-                lyrics="Anh em ta về cùng nhau ta quây quần này, 1-2-3-4-5
-                Anh em ta về cùng nhau ta sum họp này, 5-4-3-2-1
-                Một đều chân bước nhé
-                Hai quay nhìn nhau đi
-                Ba cầm tay chắc nhé, ko muốn ai chia lìa
-                Bốn nhớ rằng chúng ta bốn bể anh em một nhà
-                Năm nhớ mãi tình người trong câu ca"
-            />
-            <Song
-                title="Hát To Hát Nhỏ"
-                lyrics="Ta hát to hát nhỏ nhỏ nhỏ
-                Rồi mình ngồi kể chuyện cho nhau nghe
-                Ta vui ca hát hát cho vui đời ta
-                Ta vui ca hát hát cho vui đời ta.
-                Ta hát to hát nhỏ nhỏ nhỏ
-                Rồi mình ngồi kể chuyện cho nhau nghe
-                Ta vui ca hát hát cho vui đời ta
-                Ta vui ca hát hát cho vui đời ta."
-            />
-            <Song
-                title="Dây Thân Ái"
-                lyrics="Dây thân ái lan rộng muôn nhà
-                Tay sắp xa nhưng tim không xa
-                Vui tươi ta biết trong lòng nhớ lòng
-                Ca hát trong không gian đơm hoa
-                Ðường tuy xa nhưng tình bao la
-                Tiến bước theo hương thơm nhà lưu truyền
-                Dù cách xa ngàn dặm nhưng gần
-                Gang thép ta chia tay đừng buồn"
-            />
+        <div className='min-h-screen flex flex-col sm:pt-[8.333vh] text-center'>
+            <div className='flex-1 space-y-3'>
+                <Song 
+                    title="Trầm Hương Đốt"
+                    lyrics="Trầm hương đốt, xông ngát mười phương
+                    Nguyện nguyện kính đức Nghiêm Từ vô lượng
+                    Cầu cầu xin chứng tâm thành chúng con
+                    Vần vần khói kết mây lành cúng dường
+                    Ðạo nhiệm màu đã lang truyền nơi cùng nơi
+                    Nhờ chân lý chúng sanh đều thoát luân hồi
+                    Ðồng qúy kính qùy dưới đài sen
+                    Dâng hoa thơm tinh khiết màu thắm tươi cành
+                    Ðài quang minh sáng huy hoàng trang nghiêm
+                    Ơn mười phương điều ngự hào quang an lành
+                    Nhìn đạo uyển chuyễn soi khắp cùng quần sanh
+                    Phật đạo đồng cùng nhau tu tin tấn mau viên thành
+                    Nam mô Bổn Sư Thích Ca Mâu Ni Phật
+                    Nam mô Bổn Sư Thích Ca Mâu Ni Phật"
+                />
+                <Song
+                    title="Bốn Phương Trời"
+                    lyrics="Bốn phương trời ta về đây chung vui,
+                    Không phân chia giọng nói tiếng cười,
+                    Cùng nắm tay ta kết tình thân ái,
+                    Trao cho nhau những gì thiết tha (2 lần)"
+                />
+                <Song 
+                    title="Anh Em Ta Về"
+                    lyrics="Anh em ta về cùng nhau ta quây quần này, 1-2-3-4-5
+                    Anh em ta về cùng nhau ta sum họp này, 5-4-3-2-1
+                    Một đều chân bước nhé
+                    Hai quay nhìn nhau đi
+                    Ba cầm tay chắc nhé, ko muốn ai chia lìa
+                    Bốn nhớ rằng chúng ta bốn bể anh em một nhà
+                    Năm nhớ mãi tình người trong câu ca"
+                />
+                <Song
+                    title="Hát To Hát Nhỏ"
+                    lyrics="Ta hát to hát nhỏ nhỏ nhỏ
+                    Rồi mình ngồi kể chuyện cho nhau nghe
+                    Ta vui ca hát hát cho vui đời ta
+                    Ta vui ca hát hát cho vui đời ta.
+                    Ta hát to hát nhỏ nhỏ nhỏ
+                    Rồi mình ngồi kể chuyện cho nhau nghe
+                    Ta vui ca hát hát cho vui đời ta
+                    Ta vui ca hát hát cho vui đời ta."
+                />
+                <Song
+                    title="Dây Thân Ái"
+                    lyrics="Dây thân ái lan rộng muôn nhà
+                    Tay sắp xa nhưng tim không xa
+                    Vui tươi ta biết trong lòng nhớ lòng
+                    Ca hát trong không gian đơm hoa
+                    Ðường tuy xa nhưng tình bao la
+                    Tiến bước theo hương thơm nhà lưu truyền
+                    Dù cách xa ngàn dặm nhưng gần
+                    Gang thép ta chia tay đừng buồn"
+                />
+            </div>
             <Footer />
         </div>
     )
