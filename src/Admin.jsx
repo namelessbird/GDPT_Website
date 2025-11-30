a95ebfcd138f790dfac7bcb1e0efdbca7cb3dcc4
Ryan <ryup2544@gmail.com>
2025-11-30 01:30:41 -0800

    styled login page

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -10,19 +10,20 @@ export default function Admin(){
     }
 
     return(
-        <div className="bg-[#384264] h-screen flex items-center justify-center">
-            <form onSubmit={handleSubmit} className="flex flex-col text-center content-center bg-white rounded-md px-8 py-20">
-                <h2>Admin Only</h2>
-                <label>
-                    <input type="text" placeholder="username" value={user}
-                    onChange={(e) => setUser(e.target.value)}></input>
+        <div className="bg-[#384264] h-screen flex flex-col items-center justify-center">
+            <form onSubmit={handleSubmit} className="relative flex flex-col text-center content-center bg-white rounded-md px-8 pt-20 pb-10 sm:w-[30%] sm:max-w-96 sm:min-w-64">
+                <h2 className="absolute top-5 left-1/2 -translate-x-1/2 text-xl">Admin Only</h2>
+                <label className="sm:-mx-2">
+                    <input className="border-gray-200 border-2 rounded pl-2 py-1 sm:w-[100%]" type="text" placeholder="username" value={user}
+                    onChange={(e) => setUser(e.target.value)} required></input>
                 </label>
-                <label>
-                    <input type="password" placeholder="password" value={pass}
-                    onChange={(e) => setPass(e.target.value)}></input>
+                <label className="pt-6 pb-6 sm:-mx-2">
+                    <input className="border-gray-200 border-2 rounded pl-2 py-1 sm:w-[100%]" type="password" placeholder="password" value={pass}
+                    onChange={(e) => setPass(e.target.value)} required></input>
                 </label>
-                <button type="submit">Log In</button>
+                <button type="submit" className="text-white bg-[#384264] cursor-pointer py-1.5 mx-[25%] rounded-md hover:brightness-80">Log In</button>
             </form>
+            <a className="text-white cursor-pointer underline pt-2.5">Go back to Home</a>
         </div>
     )
 }
